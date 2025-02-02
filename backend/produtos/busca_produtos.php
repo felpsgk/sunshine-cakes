3fe1bc390b8f13b1f460df3763780cede42850a8
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-02 13:43:21 -0300

    pdo

--- backend/produtos/busca_produtos.php
+++ backend/produtos/busca_produtos.php
@@ -1,9 +1,12 @@
 <?php
-include '../db/start_db_conn.php';
-// Busca os produtos cadastrados
-$stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
-$stmt->execute();
-$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r("" . $produtos . "");
-return $produtos;
+function buscarProdutos()
+{
+    include '../db/start_db_conn.php';
+    // Busca os produtos cadastrados
+    $stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
+    $stmt->execute();
+    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print ("" . $produtos . "");
+    return $produtos;
+}
 ?>
--- backend/produtos/insere_produtos.php
+++ backend/produtos/insere_produtos.php
@@ -1,15 +1,18 @@
 <?php
-include '../db/start_db_conn.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nome_produto'], $_POST['preco'], $_POST['quantidade'])) {
-    $nome = $_POST['nome_produto'];
-    $preco = $_POST['preco'];
-    $quantidade = $_POST['quantidade'];
+function insereProduto()
+{
+    include '../db/start_db_conn.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nome_produto'], $_POST['preco'], $_POST['quantidade'])) {
+        $nome = $_POST['nome_produto'];
+        $preco = $_POST['preco'];
+        $quantidade = $_POST['quantidade'];
 
-    $stmt = $pdo->prepare("INSERT INTO produtos (nome, preco, quantidade) VALUES (?, ?, ?)");
-    if ($stmt->execute([$nome, $preco, $quantidade])) {
-        echo "<script>alert('Produto cadastrado com sucesso!');</script>";
-    } else {
-        echo "<script>alert('Erro ao cadastrar o produto.');</script>";
+        $stmt = $pdo->prepare("INSERT INTO produtos (nome, preco, quantidade) VALUES (?, ?, ?)");
+        if ($stmt->execute([$nome, $preco, $quantidade])) {
+            echo "<script>alert('Produto cadastrado com sucesso!');</script>";
+        } else {
+            echo "<script>alert('Erro ao cadastrar o produto.');</script>";
+        }
     }
 }
 ?>
