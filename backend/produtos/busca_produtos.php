7117abaa18599c72867f56a5dc19598ea3880235
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-02 13:57:28 -0300

    no funct

--- backend/produtos/busca_produtos.php
+++ backend/produtos/busca_produtos.php
@@ -1,12 +1,10 @@
 <?php
-function buscarProdutos()
-{
-    include '../db/start_db_conn.php';
-    // Busca os produtos cadastrados
-    $stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
-    $stmt->execute();
-    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    print_r($produtos);
-    return $produtos;
-}
+include '../db/start_db_conn.php';
+// Busca os produtos cadastrados
+$stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
+$stmt->execute();
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($produtos);
+return $produtos;
+
 ?>
