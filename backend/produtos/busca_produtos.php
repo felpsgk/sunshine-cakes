96f3869a0313b3894e59712b6f9f2ed262838568
trc6018386 <trc6018386@unimedbh.com.br>
2025-02-02 17:04:28 -0300

    teste3

--- backend/produtos/busca_produtos.php
+++ backend/produtos/busca_produtos.php
@@ -1,16 +1,14 @@
 <?php
 include '../db/start_db_conn.php';
 
-global $pdo;
-// Busca os produtos cadastrados
 $stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
 $stmt->execute();
 $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 if (!$produtos) {
     echo json_encode(["erro" => "Erro na consulta ou tabela vazia."]);
     exit();
 }
 
-return json_encode($produtos);
-
+echo json_encode($produtos);
 ?>
