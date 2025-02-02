4243ca32a1871130849f2b29a0eaf6ffae2927f1
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-02 13:45:42 -0300

    log10

--- backend/produtos/busca_produtos.php
+++ backend/produtos/busca_produtos.php
@@ -7,7 +7,7 @@ function buscarProdutos()
     $stmt = $pdo->prepare("SELECT * FROM produtos ORDER BY id DESC");
     $stmt->execute();
     $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    print ("" . $produtos . "");
+    print_r($produtos);
     return $produtos;
 }
 ?>
