450e182a705b7ef6c39e93f404bcf13949887268
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-02 14:17:23 -0300

    log19

--- backend/produtos/insere_produtos.php
+++ backend/produtos/insere_produtos.php
@@ -1,19 +1,19 @@
 <?php
 include '../db/start_db_conn.php';
-function insereProduto()
-{
-    global $pdo;
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nome_produto'], $_POST['preco'], $_POST['peso'])) {
-        $nome = $_POST['nome_produto'];
-        $preco = $_POST['preco'];
-        $peso = $_POST['peso'];
+global $pdo;
+echo "chegou aqui";
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nome_produto'], $_POST['preco'], $_POST['peso'])) {
+    echo "entrou if";
+    $nome = $_POST['nome_produto'];
+    $preco = $_POST['preco'];
+    $peso = $_POST['peso'];
 
-        $stmt = $pdo->prepare("INSERT INTO produtos (nome, preco, peso) VALUES (?, ?, ?)");
-        if ($stmt->execute([$nome, $preco, $peso])) {
-            echo "<script>alert('Produto cadastrado com sucesso!');</script>";
-        } else {
-            echo "<script>alert('Erro ao cadastrar o produto.');</script>";
-        }
+    $stmt = $pdo->prepare("INSERT INTO produtos (nome, preco, peso) VALUES (?, ?, ?)");
+    if ($stmt->execute([$nome, $preco, $peso])) {
+        echo "<script>alert('Produto cadastrado com sucesso!');</script>";
+    } else {
+        echo "<script>alert('Erro ao cadastrar o produto.');</script>";
     }
 }
+
 ?>
