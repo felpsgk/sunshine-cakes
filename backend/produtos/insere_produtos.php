9e9538394c16f481908c32974bec4faac42f37f5
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-02 14:18:39 -0300

    log20

--- backend/produtos/insere_produtos.php
+++ backend/produtos/insere_produtos.php
@@ -1,9 +1,7 @@
 <?php
 include '../db/start_db_conn.php';
 global $pdo;
-echo "chegou aqui";
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nome_produto'], $_POST['preco'], $_POST['peso'])) {
-    echo "entrou if";
     $nome = $_POST['nome_produto'];
     $preco = $_POST['preco'];
     $peso = $_POST['peso'];
